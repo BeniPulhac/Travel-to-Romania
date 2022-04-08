43c3c34b4835277bbf7e676c5ac026adcef05af0
benyl <beny.liviu19@gmail.com>
2022-04-08 14:19:34 +0300

    Update 8/04/21

--- hotels.php
+++ hotels.php
@@ -7,110 +7,125 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-    <link rel = "stylesheet" href = "css/hotels.css">
-    <link rel = "stylesheet" href = "css/header.css">
-    <link rel = "stylesheet" href = "css/footer.css">
+
+    <link rel = "stylesheet" href = "assets/css/hotels.css">
+    <link rel = "stylesheet" href = "assets/css/nav-bar.css">
+    <link rel = "stylesheet" href = "assets/css/header.css">
+    <link rel = "stylesheet" href = "assets/css/footer.css">
 </head>
 <body>
 <div class = "hero-img"></div>
     <header>
-        <img src = "images/flag-logo.jpg" class = "logo" alt = "Romania">
+        <img src="assets/images/flag-logo.jpg" class="logo" alt="Romania">
 
-        <nav class="nav-ul">
-            <svg class="close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 2C10.0222 2 8.08879 2.58649 6.4443 3.6853C4.79981 4.78412 3.51809 6.3459 2.76121 8.17317C2.00433 10.0004 1.8063 12.0111 2.19215 13.9509C2.578 15.8907 3.53041 17.6725 4.92894 19.0711C6.32746 20.4696 8.10929 21.422 10.0491 21.8079C11.9889 22.1937 13.9996 21.9957 15.8268 21.2388C17.6541 20.4819 19.2159 19.2002 20.3147 17.5557C21.4135 15.9112 22 13.9778 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7363 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2V2ZM14.71 13.29C14.8037 13.383 14.8781 13.4936 14.9289 13.6154C14.9797 13.7373 15.0058 13.868 15.0058 14C15.0058 14.132 14.9797 14.2627 14.9289 14.3846C14.8781 14.5064 14.8037 14.617 14.71 14.71C14.617 14.8037 14.5064 14.8781 14.3846 14.9289C14.2627 14.9797 14.132 15.0058 14 15.0058C13.868 15.0058 13.7373 14.9797 13.6154 14.9289C13.4936 14.8781 13.383 14.8037 13.29 14.71L12 13.41L10.71 14.71C10.617 14.8037 10.5064 14.8781 10.3846 14.9289C10.2627 14.9797 10.132 15.0058 10 15.0058C9.86799 15.0058 9.73729 14.9797 9.61543 14.9289C9.49357 14.8781 9.38297 14.8037 9.29 14.71C9.19628 14.617 9.12188 14.5064 9.07111 14.3846C9.02034 14.2627 8.99421 14.132 8.99421 14C8.99421 13.868 9.02034 13.7373 9.07111 13.6154C9.12188 13.4936 9.19628 13.383 9.29 13.29L10.59 12L9.29 10.71C9.1017 10.5217 8.99591 10.2663 8.99591 10C8.99591 9.7337 9.1017 9.4783 9.29 9.29C9.47831 9.1017 9.7337 8.99591 10 8.99591C10.2663 8.99591 10.5217 9.1017 10.71 9.29L12 10.59L13.29 9.29C13.4783 9.1017 13.7337 8.99591 14 8.99591C14.2663 8.99591 14.5217 9.1017 14.71 9.29C14.8983 9.4783 15.0041 9.7337 15.0041 10C15.0041 10.2663 14.8983 10.5217 14.71 10.71L13.41 12L14.71 13.29Z" fill="black"/>
-            </svg>
+        <div class="log-in">
+            <!--            Change this div later, using JavaScript-->
+            <!--        <button id="logIn" >Log In / Register</button>-->
+            <form action="log-in.php">
+                <input type="submit" value="Log In">
+            </form>
 
-            <ul class="nav-ul">
-                <li><a href = "index.php" class="nav-link">Home</a></li>
-                <li><a href = "hotels.php" class="nav-link">Hotels</a></li>
-                <li><a href = "attractions.php" class="nav-link">Attractions</a></li>
-                <li><a href = "contact.php" class="nav-link">Contact</a></li>
-                <li><a href = "about.php" class="nav-link">About</a></li>
-            </ul>
-        </nav>
+            <form action="log-in.php">
+                <input type="submit" value="Sign Up">
+            </form>
+        </div>
 
         <svg class="menu" viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M24 32H0V26.6667H24V32ZM48 18.6667H0V13.3333H48V18.6667ZM48 5.33333H24V0H48V5.33333Z" fill="white"/>
+            <path d="M24 32H0V26.6667H24V32ZM48 18.6667H0V13.3333H48V18.6667ZM48 5.33333H24V0H48V5.33333Z" fill="black"/>
         </svg>
-
     </header>
 
-    <div class = "tab-check-in">
-        <div class="check-in">
-            <button id = "checkIn">Check In</button>
+    <nav class="nav-ul">
+        <svg class="close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 2C10.0222 2 8.08879 2.58649 6.4443 3.6853C4.79981 4.78412 3.51809 6.3459 2.76121 8.17317C2.00433 10.0004 1.8063 12.0111 2.19215 13.9509C2.578 15.8907 3.53041 17.6725 4.92894 19.0711C6.32746 20.4696 8.10929 21.422 10.0491 21.8079C11.9889 22.1937 13.9996 21.9957 15.8268 21.2388C17.6541 20.4819 19.2159 19.2002 20.3147 17.5557C21.4135 15.9112 22 13.9778 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7363 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2V2ZM14.71 13.29C14.8037 13.383 14.8781 13.4936 14.9289 13.6154C14.9797 13.7373 15.0058 13.868 15.0058 14C15.0058 14.132 14.9797 14.2627 14.9289 14.3846C14.8781 14.5064 14.8037 14.617 14.71 14.71C14.617 14.8037 14.5064 14.8781 14.3846 14.9289C14.2627 14.9797 14.132 15.0058 14 15.0058C13.868 15.0058 13.7373 14.9797 13.6154 14.9289C13.4936 14.8781 13.383 14.8037 13.29 14.71L12 13.41L10.71 14.71C10.617 14.8037 10.5064 14.8781 10.3846 14.9289C10.2627 14.9797 10.132 15.0058 10 15.0058C9.86799 15.0058 9.73729 14.9797 9.61543 14.9289C9.49357 14.8781 9.38297 14.8037 9.29 14.71C9.19628 14.617 9.12188 14.5064 9.07111 14.3846C9.02034 14.2627 8.99421 14.132 8.99421 14C8.99421 13.868 9.02034 13.7373 9.07111 13.6154C9.12188 13.4936 9.19628 13.383 9.29 13.29L10.59 12L9.29 10.71C9.1017 10.5217 8.99591 10.2663 8.99591 10C8.99591 9.7337 9.1017 9.4783 9.29 9.29C9.47831 9.1017 9.7337 8.99591 10 8.99591C10.2663 8.99591 10.5217 9.1017 10.71 9.29L12 10.59L13.29 9.29C13.4783 9.1017 13.7337 8.99591 14 8.99591C14.2663 8.99591 14.5217 9.1017 14.71 9.29C14.8983 9.4783 15.0041 9.7337 15.0041 10C15.0041 10.2663 14.8983 10.5217 14.71 10.71L13.41 12L14.71 13.29Z" fill="black"/>
+        </svg>
 
-            <div class="calendar">
-                <div class="header-calendar">
-                    <span>Select a date to Check In</span>
-                    <hr>
-                    <div class="top-calendar">
-                        <button id = "left"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg></button>
-                        <div class="calendar-month" id = "calendarMonth" aria-live="polite">
-                            Aprilie 2022
-                        </div>
-                        <button id = "right"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg></button>
-                    </div>
-                    <div class="calendar-check-in">
-                        <table class = "calendar-dates" aria-labelledby="calendar-month">
-                            <thead class = "calendar-row">
-                                <tr>
-                                    <th scope = "col" class = "calendar-day-name">Mo</th>
-                                    <th scope = "col" class = "calendar-day-name">Tu</th>
-                                    <th scope = "col" class = "calendar-day-name">We</th>
-                                    <th scope = "col" class = "calendar-day-name">Th</th>
-                                    <th scope = "col" class = "calendar-day-name">Fr</th>
-                                    <th scope = "col" class = "calendar-day-name">Sa</th>
-                                    <th scope = "col" class = "calendar-day-name">Su</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr></tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>
-                </div>
+        <ul class="nav-ul">
+            <li><a href = "index.php" class="nav-link">Home</a></li>
+            <li><a href = "hotels.php" class="nav-link">Hotels</a></li>
+            <li><a href = "attractions.php" class="nav-link">Attractions</a></li>
+            <li><a href = "contact.php" class="nav-link">Contact</a></li>
+            <li><a href = "about.php" class="nav-link">About</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <div class = "sub-meniu-bar">
+            <div class="check-in">
+                <button id = "checkIn">Check In</button>
+
+<!--                <div class="calendar">-->
+<!--                    <div class="header-calendar">-->
+<!--                        <span>Select a date to Check In</span>-->
+<!--                        <hr>-->
+<!--                        <div class="top-calendar">-->
+<!--                            <button id = "left"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg></button>-->
+<!--                            <div class="calendar-month" id = "calendarMonth" aria-live="polite">-->
+<!--                                Aprilie 2022-->
+<!--                            </div>-->
+<!--                            <button id = "right"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg></button>-->
+<!--                        </div>-->
+<!--                        <div class="calendar-check-in">-->
+<!--                            <table class = "calendar-dates" aria-labelledby="calendar-month">-->
+<!--                                <thead class = "calendar-row">-->
+<!--                                    <tr>-->
+<!--                                        <th scope = "col" class = "calendar-day-name">Mo</th>-->
+<!--                                        <th scope = "col" class = "calendar-day-name">Tu</th>-->
+<!--                                        <th scope = "col" class = "calendar-day-name">We</th>-->
+<!--                                        <th scope = "col" class = "calendar-day-name">Th</th>-->
+<!--                                        <th scope = "col" class = "calendar-day-name">Fr</th>-->
+<!--                                        <th scope = "col" class = "calendar-day-name">Sa</th>-->
+<!--                                        <th scope = "col" class = "calendar-day-name">Su</th>-->
+<!--                                    </tr>-->
+<!--                                </thead>-->
+<!--                                <tbody>-->
+<!--                                    <tr></tr>-->
+<!--                                </tbody>-->
+<!--                            </table>-->
+<!--                        </div>-->
+<!--                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>-->
+<!--                    </div>-->
+<!--                </div>-->
+
             </div>
 
-        </div>
+            <div class="check-out">
+                <button id = "checkOut">Check Out</button>
 
-        <div class="checkOut">
-            <button id = "checkOut">Check Out</button>
+<!--                <div class="calendar">-->
+<!--                    <div class="top-calendar">-->
+<!--                        <span>Select a date to Check Out</span>-->
+<!--                        <hr>-->
+<!--                        <div class="calendar-check-in">-->
+<!---->
+<!--                        </div>-->
+<!--                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>-->
+<!--                    </div>-->
+<!--                </div>-->
 
-            <div class="calendar">
-                <div class="top-calendar">
-                    <span>Select a date to Check Out</span>
-                    <hr>
-                    <div class="calendar-check-in">
-
-                    </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>
-                </div>
             </div>
 
+            <div class="guests">
+                <button id = "guests">Guests</button>
+
+            </div>
+
+            <div class="search">
+                <button id = "search">Search</button>
+            </div>
         </div>
 
-        <div class="guests">
-            <button id = "guests">Guests</button>
+        <div class="destinations">
 
         </div>
 
-        <div class="search">
-            <button id = "search">Search</button>
+        <div class="top-picks">
+
         </div>
-    </div>
-
-    <div class="destinations">
-
-    </div>
-
-    <div class="top-picks">
-
-    </div>
+    </main>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <footer>
 
+    <footer>
         <div class="footer-text">
             <span>Travel to Romania is a webside designe to help people navigate easier through places to visit, hotels to stay in and all kind of activities to do all around Romania</span>
         </div>
@@ -127,6 +142,7 @@
             </ul>
         </div>
     </footer>
-<script src = "js/main.js"></script>
+
+<script src = "assets/js/main.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,52 +8,57 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel = "stylesheet" href = "assets/css/nav-bar.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
-    <div class="hero-img"></div>
-
     <header>
-        <img src="images/flag-logo.jpg" class="logo" alt="Romania">
+        <img src="assets/images/flag-logo.jpg" class="logo" alt="Romania">
 
-        <nav>
-            <svg class="close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 2C10.0222 2 8.08879 2.58649 6.4443 3.6853C4.79981 4.78412 3.51809 6.3459 2.76121 8.17317C2.00433 10.0004 1.8063 12.0111 2.19215 13.9509C2.578 15.8907 3.53041 17.6725 4.92894 19.0711C6.32746 20.4696 8.10929 21.422 10.0491 21.8079C11.9889 22.1937 13.9996 21.9957 15.8268 21.2388C17.6541 20.4819 19.2159 19.2002 20.3147 17.5557C21.4135 15.9112 22 13.9778 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7363 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2V2ZM14.71 13.29C14.8037 13.383 14.8781 13.4936 14.9289 13.6154C14.9797 13.7373 15.0058 13.868 15.0058 14C15.0058 14.132 14.9797 14.2627 14.9289 14.3846C14.8781 14.5064 14.8037 14.617 14.71 14.71C14.617 14.8037 14.5064 14.8781 14.3846 14.9289C14.2627 14.9797 14.132 15.0058 14 15.0058C13.868 15.0058 13.7373 14.9797 13.6154 14.9289C13.4936 14.8781 13.383 14.8037 13.29 14.71L12 13.41L10.71 14.71C10.617 14.8037 10.5064 14.8781 10.3846 14.9289C10.2627 14.9797 10.132 15.0058 10 15.0058C9.86799 15.0058 9.73729 14.9797 9.61543 14.9289C9.49357 14.8781 9.38297 14.8037 9.29 14.71C9.19628 14.617 9.12188 14.5064 9.07111 14.3846C9.02034 14.2627 8.99421 14.132 8.99421 14C8.99421 13.868 9.02034 13.7373 9.07111 13.6154C9.12188 13.4936 9.19628 13.383 9.29 13.29L10.59 12L9.29 10.71C9.1017 10.5217 8.99591 10.2663 8.99591 10C8.99591 9.7337 9.1017 9.4783 9.29 9.29C9.47831 9.1017 9.7337 8.99591 10 8.99591C10.2663 8.99591 10.5217 9.1017 10.71 9.29L12 10.59L13.29 9.29C13.4783 9.1017 13.7337 8.99591 14 8.99591C14.2663 8.99591 14.5217 9.1017 14.71 9.29C14.8983 9.4783 15.0041 9.7337 15.0041 10C15.0041 10.2663 14.8983 10.5217 14.71 10.71L13.41 12L14.71 13.29Z"
-                      fill="black"/>
-            </svg>
-
-            <ul class="nav-ul">
-                <li><a href="index.php" class="nav-link">Home</a></li>
-                <li><a href="hotels.php" class="nav-link">Hotels</a></li>
-                <li><a href="attractions.php" class="nav-link">Attractions</a></li>
-                <li><a href="contact.php" class="nav-link">Contact</a></li>
-                <li><a href="about.php" class="nav-link">About</a></li>
-            </ul>
-        </nav>
-
-        <svg class="menu" viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M24 32H0V26.6667H24V32ZM48 18.6667H0V13.3333H48V18.6667ZM48 5.33333H24V0H48V5.33333Z" fill="white"/>
-        </svg>
-    </header>
-
-    <main>
         <div class="log-in">
-<!--            Change this div later, using JavaScript-->
-<!--        <button id="logIn" >Log In / Register</button>-->
+            <!--            Change this div later, using JavaScript-->
+            <!--        <button id="logIn" >Log In / Register</button>-->
             <form action="log-in.php">
-                <input type="submit" value="Log in / Sing Up">
+                <input type="submit" value="Log In">
+            </form>
+
+            <form action="log-in.php">
+                <input type="submit" value="Sign Up">
             </form>
         </div>
 
-        <div class="presentation">
-            <h1>Do you want to travel to Romania?</h1>
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad, alias commodi dolores error eum
-                eveniet fugit impedit ipsam laborum minus modi natus omnis quisquam quo, ratione similique vel vero.</p>
-        </div>
+        <svg class="menu" viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M24 32H0V26.6667H24V32ZM48 18.6667H0V13.3333H48V18.6667ZM48 5.33333H24V0H48V5.33333Z" fill="black"/>
+        </svg>
+    </header>
 
+    <div class="hero-img"></div>
+
+    <nav>
+        <svg class="close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 2C10.0222 2 8.08879 2.58649 6.4443 3.6853C4.79981 4.78412 3.51809 6.3459 2.76121 8.17317C2.00433 10.0004 1.8063 12.0111 2.19215 13.9509C2.578 15.8907 3.53041 17.6725 4.92894 19.0711C6.32746 20.4696 8.10929 21.422 10.0491 21.8079C11.9889 22.1937 13.9996 21.9957 15.8268 21.2388C17.6541 20.4819 19.2159 19.2002 20.3147 17.5557C21.4135 15.9112 22 13.9778 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7363 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2V2ZM14.71 13.29C14.8037 13.383 14.8781 13.4936 14.9289 13.6154C14.9797 13.7373 15.0058 13.868 15.0058 14C15.0058 14.132 14.9797 14.2627 14.9289 14.3846C14.8781 14.5064 14.8037 14.617 14.71 14.71C14.617 14.8037 14.5064 14.8781 14.3846 14.9289C14.2627 14.9797 14.132 15.0058 14 15.0058C13.868 15.0058 13.7373 14.9797 13.6154 14.9289C13.4936 14.8781 13.383 14.8037 13.29 14.71L12 13.41L10.71 14.71C10.617 14.8037 10.5064 14.8781 10.3846 14.9289C10.2627 14.9797 10.132 15.0058 10 15.0058C9.86799 15.0058 9.73729 14.9797 9.61543 14.9289C9.49357 14.8781 9.38297 14.8037 9.29 14.71C9.19628 14.617 9.12188 14.5064 9.07111 14.3846C9.02034 14.2627 8.99421 14.132 8.99421 14C8.99421 13.868 9.02034 13.7373 9.07111 13.6154C9.12188 13.4936 9.19628 13.383 9.29 13.29L10.59 12L9.29 10.71C9.1017 10.5217 8.99591 10.2663 8.99591 10C8.99591 9.7337 9.1017 9.4783 9.29 9.29C9.47831 9.1017 9.7337 8.99591 10 8.99591C10.2663 8.99591 10.5217 9.1017 10.71 9.29L12 10.59L13.29 9.29C13.4783 9.1017 13.7337 8.99591 14 8.99591C14.2663 8.99591 14.5217 9.1017 14.71 9.29C14.8983 9.4783 15.0041 9.7337 15.0041 10C15.0041 10.2663 14.8983 10.5217 14.71 10.71L13.41 12L14.71 13.29Z"
+                  fill="black"/>
+        </svg>
+
+        <ul class="nav-ul">
+            <li><a href="index.php" class="nav-link">Home</a></li>
+            <li><a href="hotels.php" class="nav-link">Hotels</a></li>
+            <li><a href="attractions.php" class="nav-link">Attractions</a></li>
+            <li><a href="contact.php" class="nav-link">Contact</a></li>
+            <li><a href="about.php" class="nav-link">About</a></li>
+        </ul>
+
+    </nav>
+
+    <div class="presentation">
+        <h1>Do you want to travel to Romania?</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad, alias commodi dolores error eum eveniet fugit impedit ipsam laborum minus modi natus omnis quisquam quo, ratione similique vel vero.</p>
+    </div>
+
+    <main>
         <div class="recently-search">
             <h2>You recently searched</h2>
             <div class="container-search">
@@ -68,6 +73,7 @@
             <div class="container-travel">
                 <div class="cnt-col1">
                     <p class="sub-title">Keep yourself and others safe</p>
+                    <hr>
                     <ul class="travel-ul">
                         <li class="list">If you develop symptoms or test positive for COVID-19, self-isolate until you recover.</li>
                         <li class="list">Wear a properly fitted mask when physical distancing is not possible and in poorly ventilated settings.</li>
@@ -76,6 +82,7 @@
                 </div>
                 <div class="cnt-col2">
                     <p class="sub-title">What to do if you feel unwell</p>
+                    <hr>
                     <ul class="travel-ul">
                         <li class="list">If you have a fever, cough and difficulty breathing, seek medical attention immediately. Call by telephone first and follow the directions of your local health authority.</li>
                         <li class="list">Know the full range of symptoms of COVID-19. The most common symptoms of COVID-19 are fever, dry cough, tiredness and loss of taste or smell. Less common symptoms include aches and pains, headache, sore throat, red or irritated eyes, diarrhoea,  a skin rash or discolouration of fingers or toes.</li>
@@ -87,7 +94,7 @@
         </div>
     </main>
 
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
     <footer>
         <div class="footer-text">
@@ -106,6 +113,6 @@
             </ul>
         </div>
     </footer>
-<script src="js/main.js"></script>
+<script src="assets/js/main.js"></script>
 </body>
 </html>
