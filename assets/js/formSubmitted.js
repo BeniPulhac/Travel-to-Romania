c0c5b7b4365072da3d59d0ce45082bf8073bc9b3
benyl <beny.liviu19@gmail.com>
2022-04-23 15:38:53 +0300

    Update - 23/04/21-15:38

--- assets/js/formSubmitted.js
+++ assets/js/formSubmitted.js
@@ -1,14 +1,32 @@
 
 
-function myFunction() {
-    var a = document.getElementById('myForm').elements[0].value;
-    var b = document.getElementById('myForm').elements[1].value;
-    var c = document.getElementById('myForm').elements[2].value;
-    var d = document.getElementById('myForm').elements[3].value;
+// function myFunction() {
+//     var a = document.getElementById('myForm').elements[0].value;
+//     var b = document.getElementById('myForm').elements[1].value;
+//     var c = document.getElementById('myForm').elements[2].value;
+//     var d = document.getElementById('myForm').elements[3].value;
+//
+//     if(a == 0 && b == 0 && c == 0 && d == 0){
+//         alert('Form Empty');
+//     }else {
+//         document.getElementById('myForm').submit(alert(a + '\n' + b + '\n' + c + '\n' + d));
+//     }
+// }
 
-    if(a == 0 && b == 0 && c == 0 && d == 0){
+// -----------------Second Way-----------------
+
+function myFunction () {
+    var x = document.getElementById('myForm');
+    var text = "";
+    var i;
+
+    for (i = 0; i < x.length - 1; i++) {
+            text = text + x.elements[i].value + '\n';
+    }
+
+    if(text != 0){
+        alert(text);
+    } else {
         alert('Form Empty');
-    }else {
-        document.getElementById('myForm').submit(alert(a + '\n' + b + '\n' + c + '\n' + d));
     }
 }
